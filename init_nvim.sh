d1268517d93f315873fb813f1cbbfb3ceb185785
wyattm124 <wyatt.maleta@gmail.com>
2024-09-16 20:01:16 -0700

    update init_nvim.sh

--- init_nvim.sh
+++ init_nvim.sh
@@ -3,12 +3,14 @@
 if ! command -v nvim &> /dev/null
 then
 	sudo apt-get update
-	sudo apt-get install gettext libgettextpo-dev
+  # if want the latest versions of fd and rg, would need arch dep package or build from source
+	sudo apt-get install gettext libgettextpo-dev ripgrep fd-find
+
 	git submodule update --init --recursive
 	cd neovim
 	git checkout v0.10.1
 	make CMAKE_BUILD_TYPE=Release
-       	#For a different location use CMAKE_INSTALL_PREFIX=/usr/local/
+  #For a different location use CMAKE_INSTALL_PREFIX=/usr/local/
 	sudo make install
 	cd ..
 fi
